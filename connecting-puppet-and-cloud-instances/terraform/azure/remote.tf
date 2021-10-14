6af8d459ccfa5dd2bab7988094e25838a1c6251d
Matthew Stone <matt@souldo.net>
2021-10-14 05:16:14 -0500

    Update remote.tf

--- connecting-puppet-and-cloud-instances/terraform/azure/remote.tf
+++ connecting-puppet-and-cloud-instances/terraform/azure/remote.tf
@@ -1,19 +1,19 @@
 data "azurerm_resource_group" "puppetize2021mj" {
-  name = "puppetize2021mj"
+  name = var.resource_group
 }
 
 data "azurerm_subnet" "puppetize2021mj" {
-  name = "puppetize2021mj"
+  name = var.subnet
   resource_group_name = data.azurerm_resource_group.puppetize2021mj.name
   virtual_network_name = data.azurerm_virtual_network.puppetize2021mj.name
 }
 
 data "azurerm_virtual_network" "puppetize2021mj" {
-  name = "puppetize2021mj"
+  name = var.virtual_network
   resource_group_name = data.azurerm_resource_group.puppetize2021mj.name
 }
 
 data "azurerm_network_security_group" "puppetize2021mj" {
-    name = "puppetize2021mj"
+    name = var.security_group
     resource_group_name = data.azurerm_resource_group.puppetize2021mj.name
 }
